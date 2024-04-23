423c50de87a426ae183e55aab9ad50acd45922b1
Andrei Tambovtsev <ait27ru@gmail.com>
2024-04-24 11:32:41 +1200

    ValidParenthesesTests: Add more tests.

--- CSharpLeetCodeTests/EasyProblems/ValidParenthesesTests.cs
+++ CSharpLeetCodeTests/EasyProblems/ValidParenthesesTests.cs
@@ -18,10 +18,33 @@ namespace CSharpLeetCodeTests.EasyProblems
             Assert.IsTrue(result);
         }
 
+        [TestMethod]
         public void IsValid_ShouldSatisfy_Example2()
         {
             // arrange
             var inputString = "()";
+            // act
+            var result = solution.IsValid(inputString);
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValid_ShouldSatisfy_Example3()
+        {
+            // arrange
+            var inputString = "(]";
+            // act
+            var result = solution.IsValid(inputString);
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_ShouldSatisfy_Example4()
+        {
+            // arrange
+            var inputString = "({[]})";
             // act
             var result = solution.IsValid(inputString);
             // assert
