204ab876fb471996bebee23a3467dbfec5ac0a5d
Andrei Tambovtsev <ait27ru@gmail.com>
2024-04-24 12:02:03 +1200

    CoinChange: Add a solution and unit tests for the problem.

---
+++ CSharpLeetCode/MediumProblems/CoinChange.cs
@@ -0,0 +1,40 @@
+﻿/*
+    https://leetcode.com/problems/coin-change/description/
+    You are given an integer array coins representing coins of different denominations and
+    an integer amount representing a total amount of money.
+    Return the fewest number of coins that you need to make up that amount.
+    If that amount of money cannot be made up by any combination of the coins, return -1.
+    You may assume that you have an infinite number of each kind of coin.
+*/
+
+namespace CSharpLeetCode.MediumProblems
+{
+    public class CoinChange
+    {
+        public int CoinChangeSolution(int[] coins, int amount)
+        {
+            var maxAmount = amount + 1;
+
+            int[] dp = new int[maxAmount];
+            Array.Fill(dp, maxAmount);
+
+            dp[0] = 0;
+
+            for (int currentAmount = 1; currentAmount < maxAmount; currentAmount++)
+            {
+                foreach (var coin in coins)
+                {
+                    if (currentAmount - coin >= 0)
+                    {
+                        dp[currentAmount] = Math.Min(dp[currentAmount], 1 + dp[currentAmount - coin]);
+                    }
+                }
+            }
+
+            if (dp[amount] == maxAmount)
+                return -1;
+            else
+                return dp[amount];
+        }
+    }
+}
---
+++ CSharpLeetCodeTests/MediumProblems/CoinChangeTests.cs
@@ -0,0 +1,52 @@
+﻿using CSharpLeetCode.MediumProblems;
+
+namespace CSharpLeetCodeTests.MediumProblems
+{
+    [TestClass()]
+    public class CoinChangeTests
+    {
+        private readonly CoinChange _coinChange = new();
+
+        [TestMethod()]
+        public void CoinChangeSolution_ShouldSatisfy_Example1()
+        {
+            // arrange
+            int[] coins = [1, 2, 5];
+            int amount = 11;
+
+            // act
+            var coinsNumber = _coinChange.CoinChangeSolution(coins, amount);
+
+            // assert
+            Assert.AreEqual(3, coinsNumber);
+        }
+
+        [TestMethod()]
+        public void CoinChangeSolution_ShouldSatisfy_Example2()
+        {
+            // arrange
+            int[] coins = [2];
+            int amount = 3;
+
+            // act
+            var coinsNumber = _coinChange.CoinChangeSolution(coins, amount);
+
+            // assert
+            Assert.AreEqual(-1, coinsNumber);
+        }
+
+        [TestMethod()]
+        public void CoinChangeSolution_ShouldSatisfy_Example3()
+        {
+            // arrange
+            int[] coins = [1];
+            int amount = 0;
+
+            // act
+            var coinsNumber = _coinChange.CoinChangeSolution(coins, amount);
+
+            // assert
+            Assert.AreEqual(0, coinsNumber);
+        }
+    }
+}
