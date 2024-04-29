fd32e7470bbeca7bc8a798907582658db896647a
Andrei Tambovtsev <ait27ru@gmail.com>
2024-04-29 14:18:55 +1000

    PerfectSquares: Add a solution and unit tests for the problem.

---
+++ CSharpLeetCode/MediumProblems/PerfectSquaresSolution.cs
@@ -0,0 +1,35 @@
+﻿/*
+    https://leetcode.com/problems/perfect-squares/description/
+    Given an integer n, return the least number of perfect square numbers that sum to n.
+    A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+    For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+*/
+
+namespace CSharpLeetCode.MediumProblems
+{
+    public class PerfectSquaresSolution
+    {
+        public int NumSquares(int n)
+        {
+            int[] dp = new int[n + 1];
+            Array.Fill(dp, n);
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int targetSquare = 1; targetSquare <= n; targetSquare++)
+            {
+                for (int side = 1; side <= targetSquare; side++)
+                {
+                    var currentSquare = side * side;
+                    var restSquare = targetSquare - currentSquare;
+                    if (restSquare < 0)
+                    {
+                        break;
+                    }
+                    dp[targetSquare] = Math.Min(dp[targetSquare], 1 + dp[restSquare]);
+                }
+            }
+            return dp[n];
+        }
+    }
+}
---
+++ CSharpLeetCodeTests/MediumProblems/PerfectSquaresSolutionTests.cs
@@ -0,0 +1,50 @@
+﻿using CSharpLeetCode.MediumProblems;
+
+namespace CSharpLeetCodeTests.MediumProblems
+{
+    [TestClass()]
+    public class PerfectSquaresSolutionTests
+    {
+        private readonly PerfectSquaresSolution _solution = new();
+
+        [TestMethod()]
+        public void NumSquares_ShouldSatisfy_MinValueEdgeCase()
+        {
+            var num = _solution.NumSquares(1);
+            // 1 = 1 (1x1)
+            Assert.AreEqual(1, num);
+        }
+
+        [TestMethod()]
+        public void NumSquares_ShouldSatisfy_MaxValueEdgeCase()
+        {
+            var num = _solution.NumSquares(10_000);
+            // 10000 = 10000 (100x100)
+            Assert.AreEqual(1, num);
+        }
+
+        [TestMethod()]
+        public void NumSquares_ShouldSatisfy_Example1()
+        {
+            var num = _solution.NumSquares(12);
+            // 12 = 4 + 4 + 4
+            Assert.AreEqual(3, num);
+        }
+
+        [TestMethod()]
+        public void NumSquares_ShouldSatisfy_Example2()
+        {
+            var num = _solution.NumSquares(13);
+            // 13 = 4 + 9
+            Assert.AreEqual(2, num);
+        }
+
+        [TestMethod()]
+        public void NumSquares_ShouldSatisfy_Example3()
+        {
+            var num = _solution.NumSquares(5);
+            // 5 = 4 + 1
+            Assert.AreEqual(2, num);
+        }
+    }
+}
