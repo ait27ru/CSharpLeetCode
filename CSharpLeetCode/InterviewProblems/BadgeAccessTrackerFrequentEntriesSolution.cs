9ea0b962494541c88ad236aea9285490442c045c
Andrei Tambovtsev <ait27ru@gmail.com>
2024-05-28 23:00:05 +1200

    BadgeAccessTrackerFrequentEntries: Added this problem's solution

---
+++ CSharpLeetCode/InterviewProblems/BadgeAccessTrackerFrequentEntriesSolution.cs
@@ -0,0 +1,73 @@
+﻿
+/*
+
+We are developing a security system for a badged-access room within our company's building and need to 
+identify employees who entered the secured room unusually frequently. 
+
+We have an unordered list of names and entry times recorded over a single day. Access times are provided 
+as four-digit numbers in 24-hour format, such as "0800" or "2250".
+
+Write a function that identifies any employee who badged into the room three or more times within a one-hour window.
+Your function should return each employee who meets this criterion, 
+along with the times they badged in during the one-hour period. 
+
+If an employee meets this condition in multiple one-hour periods, return the earliest period for that employee.
+
+ */
+
+namespace CSharpLeetCode.InterviewProblems
+{
+    public class BadgeAccessTrackerFrequentEntriesSolution
+    {
+        public List<ValueTuple<string, List<int>>> FindFrequentUsers(IEnumerable<ValueTuple<string, int>> records)
+        {
+            var result = new List<ValueTuple<string, List<int>>>();
+
+            var entryTimesByName = new Dictionary<string, List<int>>();
+
+            foreach (var (userName, entryTime) in records)
+            {
+                if (!entryTimesByName.ContainsKey(userName))
+                {
+                    entryTimesByName[userName] = new List<int>();
+                }
+                entryTimesByName[userName].Add(entryTime);
+            }
+
+            foreach (var times in entryTimesByName.Values)
+            {
+                times.Sort();                
+            }
+
+            foreach (var (userName, entryTimes) in entryTimesByName)
+            {
+                if (entryTimes.Count < 3)
+                    continue;
+
+                for (int i = 0; i <= entryTimes.Count - 3; i++)
+                {
+                    int startTime = entryTimes[i];
+
+                    for (int j = i + 2; j < entryTimes.Count; j++)
+                    {
+                        if (entryTimes[j] - startTime <= 100)
+                        {
+                            // The 3rd time from startTime is within one hour
+                            result.Add((userName, entryTimes.GetRange(i, j - i + 1)));
+                            break;
+                        }
+                        if (entryTimes[j] - startTime > 100)
+                        {
+                            // The 3rd time from startTime exceeds one hour
+                            break;
+                        }
+                    }
+
+                    // If a record exists for a user, that's enough
+                    if (result.Any(r => r.Item1 == userName)) break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CSharpLeetCodeTests/InterviewProblems/BadgeAccessTrackerFrequentEntriesSolutionTests.cs
@@ -0,0 +1,76 @@
+﻿using CSharpLeetCode.InterviewProblems;
+
+namespace CSharpLeetCodeTests.InterviewProblems
+{
+    [TestClass()]
+    public class BadgeAccessTrackerFrequentEntriesSolutionTests
+    {
+        private readonly BadgeAccessTrackerFrequentEntriesSolution _solution = new();
+
+        [TestMethod()]
+        public void FindFrequentUsers_ShouldSatisfyExample1()
+        {
+            // arrange
+            var testData = TestData.recordSet1;
+
+            // act
+            var result = _solution.FindFrequentUsers(testData);
+
+            // assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod()]
+        public void FindFrequentUsers_ShouldSatisfyExample2()
+        {
+            // arrange
+            var testData = TestData.recordSet2;
+
+            // act
+            var result = _solution.FindFrequentUsers(testData);
+
+            // assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Paul", result[0].Item1);
+            CollectionAssert.AreEqual(new List<int> { 800, 805, 810 }, result[0].Item2);
+        }
+
+        [TestMethod()]
+        public void FindFrequentUsers_ShouldSatisfyExample3()
+        {
+            // arrange
+            var testData = TestData.recordSet3;
+
+            // act
+            var result = _solution.FindFrequentUsers(testData);
+
+            // assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Paul", result[0].Item1);
+            CollectionAssert.AreEqual(new List<int> { 800, 805, 810 }, result[0].Item2);
+            Assert.AreEqual("Raj", result[1].Item1);
+            CollectionAssert.AreEqual(new List<int> { 1200, 1205, 1210 }, result[1].Item2);
+        }
+
+        private static class TestData
+        {
+            public static readonly List<ValueTuple<string, int>> recordSet1 = new()
+            {
+                ("Paul", 800)
+            };
+
+            public static readonly List<ValueTuple<string, int>> recordSet2 = new()
+            {
+                ("Paul", 800), ("Paul", 805), ("Paul", 810),
+                ("Raj", 900)
+            };
+
+            public static readonly List<ValueTuple<string, int>> recordSet3 = new()
+            {
+                ("Paul", 800), ("Paul", 805), ("Paul", 810),
+                ("Raj", 900), ("Raj", 1200), ("Raj", 1205), ("Raj", 1210)
+            };
+
+        }
+    }
+}
