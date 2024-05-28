e70d4687e938569d6022e32fe79931ca68fe780e
Andrei Tambovtsev <ait27ru@gmail.com>
2024-05-28 22:35:22 +1200

    BadgeAccessTrackerMatchEntriesSolution: Add problem's solution

---
+++ CSharpLeetCode/InterviewProblems/BadgeAccessTrackerMatchEntriesSolution.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+ * 
+We are implementing a security system for a badged-access room within our company's premises. 
+Given a sequential list of employees who used their badges to enter or exit the room, write a function that returns two collections:
+
+Employees who failed to use their badge to exit the room: These employees logged an entry without a corresponding exit.
+(All employees must exit the room before the log concludes.)
+
+Employees who failed to use their badge to enter the room: These employees logged an exit without a corresponding entry. 
+(The room is empty at the start of the log.)
+
+Each collection should only include unique entries, regardless of how many times an employee meets the criteria for inclusion.
+
+*/
+
+namespace CSharpLeetCode.InterviewProblems
+{
+    public class BadgeAccessTrackerMatchEntriesSolution
+    {
+        public ValueTuple<List<string>, List<string>> FindNonMatchingRecords(IEnumerable<ValueTuple<string, string>> records)
+        {
+            var missingUserExits = new HashSet<string>();
+            var missingUserEntries = new HashSet<string>();
+
+            var actionsByName = new Dictionary<string, string>();
+
+            foreach (var (userName, userAction) in records)
+            {
+                if (actionsByName.ContainsKey(userName))
+                {
+                    var lastUserAction = actionsByName[userName];
+
+                    if (lastUserAction == userAction)
+                    {
+                        if (userAction == "enter")
+                        {
+                            missingUserExits.Add(userName);
+                        }
+                        else
+                        {
+                            missingUserEntries.Add(userName);
+                        }                        
+                        actionsByName.Remove(userName);
+                    }
+                }
+                else
+                {
+                    if (userAction == "exit")
+                    {
+                        missingUserEntries.Add(userName);
+                    }
+                }
+
+                actionsByName[userName] = userAction;
+            }
+
+            // Handling those who entered and never exited
+            foreach (var userAction in actionsByName)
+            {
+                if (userAction.Value == "enter")
+                {
+                    missingUserExits.Add(userAction.Key);
+                }
+            }
+
+            return (missingUserExits.ToList(), missingUserEntries.ToList());
+        }
+    }
+}
---
+++ CSharpLeetCodeTests/InterviewProblems/BadgeAccessTrackerMatchEntriesSolutionTests.cs
@@ -0,0 +1,123 @@
+﻿using CSharpLeetCode.InterviewProblems;
+
+namespace CSharpLeetCodeTests.InterviewProblems
+{
+    [TestClass()]
+    public class BadgeAccessTrackerMatchEntriesSolutionTests
+    {
+        private readonly BadgeAccessTrackerMatchEntriesSolution _solution = new();
+
+        [TestMethod()]
+        public void FindNonMatchingRecords_ShouldSatisfyExample1()
+        {
+            // arrange
+            var data = TestData.recordSet1;
+            var expectedMissingExits = new HashSet<string>() { "Paul", "Joe", "Steve", "Curtis" };
+            var expectedMissingEntries = new HashSet<string>() { "Pauline", "Martha", "Curtis", "Joe" };
+
+
+            // act
+            var result = _solution.FindNonMatchingRecords(data);
+
+            // assert
+            Assert.IsTrue(expectedMissingExits.SetEquals(result.Item1.ToHashSet()));
+            Assert.IsTrue(expectedMissingEntries.SetEquals(result.Item2.ToHashSet()));
+        }
+
+        [TestMethod()]
+        public void FindNonMatchingRecords_ShouldSatisfyExample2()
+        {
+            // arrange
+            var data = TestData.recordSet2;
+            var expectedMissingExits = new HashSet<string>();
+            var expectedMissingEntries = new HashSet<string>();
+
+            // act
+            var result = _solution.FindNonMatchingRecords(data);
+
+            // assert
+            Assert.IsTrue(expectedMissingExits.SetEquals(result.Item1.ToHashSet()));
+            Assert.IsTrue(expectedMissingEntries.SetEquals(result.Item2.ToHashSet()));
+        }
+
+        [TestMethod()]
+        public void FindNonMatchingRecords_ShouldSatisfyExample3()
+        {
+            // arrange
+            var data = TestData.recordSet3;
+            var expectedMissingExits = new HashSet<string>() { "Paul" };
+            var expectedMissingEntries = new HashSet<string>() { "Paul" };
+
+            // act
+            var result = _solution.FindNonMatchingRecords(data);
+
+            // assert
+            Assert.IsTrue(expectedMissingExits.SetEquals(result.Item1.ToHashSet()));
+            Assert.IsTrue(expectedMissingEntries.SetEquals(result.Item2.ToHashSet()));
+        }
+
+        [TestMethod()]
+        public void FindNonMatchingRecords_ShouldSatisfyExample4()
+        {
+            // arrange
+            var data = TestData.recordSet4;
+            var expectedMissingExits = new HashSet<string>() { "Raj", "Paul" };
+            var expectedMissingEntries = new HashSet<string>() { "Paul" };
+
+            // act
+            var result = _solution.FindNonMatchingRecords(data);
+
+            // assert
+            Assert.IsTrue(expectedMissingExits.SetEquals(result.Item1.ToHashSet()));
+            Assert.IsTrue(expectedMissingEntries.SetEquals(result.Item2.ToHashSet()));
+        }
+        private static class TestData
+        {
+            public static readonly ValueTuple<string, string>[] recordSet1 = [
+                ("Paul", "enter"),
+                ("Pauline", "exit"),
+                ("Paul", "enter"),
+                ("Paul", "exit"),
+                ("Martha", "exit"),
+                ("Joe", "enter"),
+                ("Martha", "enter"),
+                ("Steve", "enter"),
+                ("Martha", "exit"),
+                ("Jennifer", "enter"),
+                ("Joe", "enter"),
+                ("Curtis", "exit"),
+                ("Curtis", "enter"),
+                ("Joe", "exit"),
+                ("Martha", "enter"),
+                ("Martha", "exit"),
+                ("Jennifer", "exit"),
+                ("Joe", "enter"),
+                ("Joe", "enter"),
+                ("Martha", "exit"),
+                ("Joe", "exit"),
+                ("Joe", "exit")
+                ];
+
+            public static readonly ValueTuple<string, string>[] recordSet2 = [
+                ("Paul", "enter"),
+                ("Paul", "exit")
+                ];
+
+            public static readonly ValueTuple<string, string>[] recordSet3 = [
+                ("Paul", "enter"),
+                ("Paul", "enter"),
+                ("Paul", "exit"),
+                ("Paul", "exit")
+                ];
+
+            public static readonly ValueTuple<string, string>[] recordSet4 = [
+                ("Raj", "enter"),
+                ("Paul", "enter"),
+                ("Paul", "exit"),
+                ("Paul", "exit"),
+                ("Paul", "enter"),
+                ("Raj", "enter")
+                ];
+        }
+    }
+}
