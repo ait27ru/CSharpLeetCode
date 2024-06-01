4c51f5c687880b9b73898ca7e11d3e9d1c9372c3
Andrei Tambovtsev <ait27ru@gmail.com>
2024-06-01 17:40:38 +1200

    DeliveryOrderOfGoodsInCities: Add the solution of the problem with unit tests.

---
+++ CSharpLeetCode/InterviewProblems/DeliveryOrderOfGoodsInCities.cs
@@ -0,0 +1,106 @@
+﻿/*
+
+A manufacturing company is located in a certain city. Their goods need to be shipped to other cities that are connected with bidirectional roads,
+though some cities may not be accessible because roads don't connect to them. The order of deliveries is determined first by distance, then by priority. 
+Given the number of cities, their connections via roads, and what city the manufacturing company is located in, determine the order of cities
+where the goods will be delivered.
+
+For example, let's say that the number of cities is cityNodes = 4, where cityFrom = [1, 2, 2], cityTo = [2, 3, 4], and company = 1. 
+In other words, the manufacturing company is located in city 1, and the roads run between cities 1 and 2, cities 2 and 3, and cities 2 and 4.
+
+In this case, the cities would be visited based on the following logic:
+• The closest city (or cities) is visited first. This is city 2, which is 1 unit from the manufacturing company.
+• The next-closest city (or cities) is visited next. This is city 3 and city 4. which are both 2 units from the manufacturing company.
+In this case, priority is then calculated, visiting the smaller- numbered city first (city 3) and continuing in ascending order (city 4).
+Therefore, the order is [2, 3, 4], which is the answer you would return.
+
+Another example, cityNodes = 3, where cityFrom = [1], cityTo = [2], and company = 2. 
+City 1 is located 1 unit of distance away from the manufacturing company.
+City 3 is not accessible because there are no roads connecting it to the manufacturing company's city. 
+Therefore, the answer is [1].
+
+One more example, cityNodes = 5, where cityFrom = [1, 1, 2, 3, 1], cityTo = [2, 3, 4, 5, 5], and company = 1. 
+Cities 2, 3, and 5 are all 1 unit of distance away from the manufacturing company. 
+These are visited based on priority in ascending order, so [2, 3, 5). 
+City 4 is 2 units of distance away from the manufacturing company, so it is visited next. 
+Therefore, the final order is [2, 3, 5, 4].
+
+--------------------------------
+v - vertices, e - edges.
+
+Time complexity:
+
+Create and initialise an adjacency dictionary.
+It allows to easily iterate through the neighbours of the node.
+Adjacency matrix would require iterating through all the nodes to identify a node's neighbours.
+Time: O(v)
+
+Fill the adjacency dictionary with connections. 
+Time: O(e)
+
+Perform BFS on the graph.
+Time: O(v + e) (since each node enqued and dequeued at most once and we iterate over its neighbours (edges))
+
+Return ordered list of cities.
+Time: O (v * log(v))
+
+--------------------------------
+Space Complexity:
+
+Fill the adjacency dictionary with connections.
+Space: O(v + e) since cities dictionary may store all edges
+
+Create a queue to do BFS on the graph.
+Space: O(v) since the queue can hold at most V elements
+
+Initialise distances dictionary.
+Space: O(v)
+
+ */
+
+namespace CSharpLeetCode.InterviewProblems
+{
+    public class DeliveryOrderOfGoodsInCities
+    {
+        public List<int> Order(int cityNodes, List<int> cityFrom, List<int> cityTo, int company)
+        {
+            Dictionary<int, List<int>> cities = new Dictionary<int, List<int>>();
+
+            for (int i = 1; i <= cityNodes; i++)
+            {
+                cities[i] = new List<int>();
+            }
+
+            for (int i = 0; i < cityFrom.Count; i++)
+            {
+                cities[cityFrom[i]].Add(cityTo[i]);
+                cities[cityTo[i]].Add(cityFrom[i]);
+            }
+
+            var queue = new Queue<int>([company]);
+
+            // Distance to self is 0.
+            var dist = new Dictionary<int, int> { { company, 0 } };
+
+            // Perform BFS
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+                foreach (int neighbor in cities[current])
+                {
+                    if (!dist.ContainsKey(neighbor))
+                    {
+                        dist[neighbor] = dist[current] + 1;
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            return dist.Where(kvp => kvp.Key != company)
+                                        .OrderBy(kvp => kvp.Value)
+                                        .ThenBy(kvp => kvp.Key)
+                                        .Select(kvp => kvp.Key)
+                                        .ToList();
+        }
+    }
+}
---
+++ CSharpLeetCodeTests/InterviewProblems/DeliveryOrderOfGoodsInCitiesTests.cs
@@ -0,0 +1,58 @@
+﻿using CSharpLeetCode.InterviewProblems;
+
+namespace CSharpLeetCodeTests.InterviewProblems
+{
+    [TestClass()]
+    public class DeliveryOrderOfGoodsInCitiesTests
+    {
+        private readonly DeliveryOrderOfGoodsInCities _solution = new();
+
+        [TestMethod()]
+        public void Order_TestCase1()
+        {
+            // arrange
+            var cityNodes = 4;
+            var cityFrom = new List<int> { 1, 2, 2 };
+            var cityTo = new List<int> { 2, 3, 4 };
+            var company = 1;
+
+            // act
+            var res = _solution.Order(cityNodes, cityFrom, cityTo, company);
+
+            // assert
+            CollectionAssert.AreEqual(new List<int> { 2, 3, 4 }, res);
+        }
+
+        [TestMethod()]
+        public void Order_TestCase2()
+        {
+            // arrange
+            var cityNodes = 3;
+            var cityFrom = new List<int> { 1 };
+            var cityTo = new List<int> { 2 };
+            var company = 2;
+
+            // act
+            var res = _solution.Order(cityNodes, cityFrom, cityTo, company);
+
+            // assert
+            CollectionAssert.AreEqual(new List<int> { 1 }, res);
+        }
+
+        [TestMethod()]
+        public void Order_TestCase3()
+        {
+            // arrange
+            var cityNodes = 5;
+            var cityFrom = new List<int> { 1, 1, 2, 3, 1 };
+            var cityTo = new List<int> { 2, 3, 4, 5, 5 };
+            var company = 1;
+
+            // act
+            var res = _solution.Order(cityNodes, cityFrom, cityTo, company);
+
+            // assert
+            CollectionAssert.AreEqual(new List<int> { 2, 3, 5, 4 }, res);
+        }
+    }
+}
