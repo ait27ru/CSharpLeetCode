20e53dc68609c042efe4477b0c4b82862efdb524
Andrei Tambovtsev <ait27ru@gmail.com>
2024-12-04 22:21:40 +1300

    SongPairSolution: Add a solution and unit tests for the problem.

---
+++ CSharpLeetCode/InterviewProblems/Atlassian/SongPairSolution.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Given a list of songs, find a pair in the list whose combined length is exactly 7 minutes. 
+ * Each item in the list is a song represented by its name and length in the format "mm:ss".
+ * If there are multiple pairs, you may return any one of them.
+ * 
+ */
+
+using System.Runtime.CompilerServices;
+
+namespace CSharpLeetCode.InterviewProblems.Atlassian;
+
+public class SongPairSolution
+{
+    public List<string> FindSongPair(List<Song> songs)
+    {
+        const int TargetTimeInSeconds = 7 * 60;
+
+        var timeToIndexMap = new Dictionary<int, int>();
+
+        var result = new List<string>();
+
+        for (int i = 0; i < songs.Count; i++)
+        {
+            var songLengthInSeconds = ConvertToSeconds(songs[i].Duration);
+
+            if (timeToIndexMap.TryGetValue(songLengthInSeconds, out int pairedIndex))
+            {
+                result.Add(songs[pairedIndex].Name);
+                result.Add(songs[i].Name);
+                return result;
+            }
+            var complementaryTime = TargetTimeInSeconds - songLengthInSeconds;
+            timeToIndexMap[complementaryTime] = i;
+        }
+        return result;
+    }
+
+    private static int ConvertToSeconds(string time) =>
+        time.Split(':') is { Length: 2 } timeParts
+            ? int.Parse(timeParts[0]) * 60 + int.Parse(timeParts[1])
+            : throw new FormatException("Invalid time format");
+}
+
+public record Song(string Name, string Duration);
---
+++ CSharpLeetCodeTests/InterviewProblems/Atlassian/SongPairSolutionTests.cs
@@ -0,0 +1,64 @@
+﻿using CSharpLeetCode.InterviewProblems.Atlassian;
+
+namespace CSharpLeetCodeTests.InterviewProblems.Atlassian;
+
+[TestClass()]
+public class SongPairSolutionTests
+{
+    private readonly SongPairSolution _sut = new();
+
+    [TestMethod()]
+    public void FindSongPair_WhenNoMatchingPairs_ReturnEmptyList()
+    {
+        // act
+        var result = _sut.FindSongPair(TestData.noMatchingPairs);
+
+        // assert
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [TestMethod()]
+    public void FindSongPair_WhenOneMatchingPair_ReturnIt()
+    {
+        // act
+        var result = _sut.FindSongPair(TestData.oneMatchingPair);
+
+        // assert
+        CollectionAssert.AreEqual(new List<string> { "Hey Ya!", "What's going on?" }, result);
+    }
+
+    [TestMethod()]
+    public void FindSongPair_WhenMultipleMatchingPairs_ReturnTheFirstOne()
+    {
+        // act
+        var result = _sut.FindSongPair(TestData.twoMatchingPairs);
+
+        // assert
+        CollectionAssert.AreEqual(new List<string> { "Hey Ya!", "What's going on?" }, result);
+    }
+
+}
+
+static class TestData
+{
+    public static readonly List<Song> noMatchingPairs = [
+        new Song("Hey Ya!", "3:54"),
+        new Song("Dreams", "2:34"),
+        new Song("Respect", "2:06"),
+    ];
+
+    public static readonly List<Song> oneMatchingPair = [
+        new Song("Hey Ya!", "3:54"),
+        new Song("Dreams", "2:34"),
+        new Song("What's going on?", "3:06"),
+        new Song("Respect", "2:06"),
+    ];
+
+    public static readonly List<Song> twoMatchingPairs = [
+        new Song("Hey Ya!", "3:54"),
+        new Song("Dreams", "2:34"),
+        new Song("What's going on?", "3:06"),
+        new Song("Respect", "2:06"),
+        new Song("Imagine", "3:26"),
+    ];
+}
